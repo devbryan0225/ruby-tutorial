8fb3c81b941cba6af1f395d1334897778cc201d5
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-09-07 23:37:24 +0800

    Case, if else, max.

---
+++ case.rb
@@ -0,0 +1,10 @@
+day = "wed"
+
+case day
+when "mon"
+    puts "Monday"
+when "tues"
+    puts "Tuesday"
+else
+    puts "None of them"
+end
---
+++ ifelse.rb
@@ -0,0 +1,13 @@
+dinosaur = false
+big = true
+
+
+if dinosaur and big
+    puts "happy"
+elsif big
+    puts "alright sure"
+elsif !dinosaur
+    puts "why no dinosaur"
+else
+    puts "sad"
+end
